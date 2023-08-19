8bb1dec374d6b5948dbd91f522e670051e5daab3
YashBhappa <ybhappa@gmail.com>
2023-08-19 12:56:59 +0530

    Dynamic Content Loaded from db

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        $contacts = Contact::orderBy('first_name')->get();
-        return view('contacts.index', compact('contacts'));
+        $contacts = Contact::orderBy('first_name')->where(function ($query) {
+            if ($companyId = request('company_id')) {
+                $query->where('company_id', $companyId);
+            }
+        })->paginate(10);
+        $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');
+        return view('contacts.index', compact('contacts', 'companies'));
     }
 
     public function create()
